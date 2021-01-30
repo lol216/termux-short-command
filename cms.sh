580e7d6fbf342394571847b9a7032320ea37fee8
lol216 <77780774+lol216@users.noreply.github.com>
2021-01-30 15:21:25 +0630

    Add files via upload

---
+++ cms.sh
@@ -0,0 +1,31 @@
+echo"Hello I am WKL"
+echo"make by Wint Khant Lin"
+echo"I am 13"
+echo"This script is long command to short as cmd"
+echo"support me with PUBG UC ID:5950924327"
+echo"start at 05:00PM end on 8:00"
+alias cls=\"clear\"
+alias aptupdate=\"apt update && apt upgrade\"
+alias pkgupdate=\"pkg update && pkg upgrade\"
+alias dir=\"ls\"
+alias cls=\"clear\"
+alias git=\"git clone\"
+alias storage-allow=\"termux-setup-storage\"
+alias php-install=\"pkg install php\"
+alias py install=\"pkg install python\"
+alias py2 install=\"pkg install python2\"
+alias Version=\"1:0:0\"
+alias helpcms=\"cls->clear
+aptupdate->"apt update && apt upgrade
+pkgupdate->pkg upgrade && pkg upgrade
+dir->ls
+git->git clone
+storage-allow->termux-setup-storage
+php install->pkg install php
+py install->pkg install python
+py2 install->pkg install python2
+Versios\"
+
+
+   
+    
